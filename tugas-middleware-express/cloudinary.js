9ba598e4a1b7502ef9a20fe9ca5b2f4dd36305fe
hanunsals <hanunsalsabila0202@gmail.com>
2024-11-07 17:09:17 +0700

    update

--- tugas-middleware-express/cloudinary.js
+++ tugas-middleware-express/cloudinary.js
@@ -2,8 +2,8 @@ const cloudinary = require("cloudinary");
 
 cloudinary.config({
   cloud_name: "dcbiegm4s", 
-  api_key: "678817664313199", 
-  api_secret: "D4z7d0DlqNi9uOP_i04G9Csnu8c", 
+  api_key: "", 
+  api_secret: "", 
 });
 
 module.exports = cloudinary;
